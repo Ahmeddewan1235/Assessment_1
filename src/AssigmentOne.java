14702a912aab06e540e1c4b487fb5dcb3d96ee30
Ahmeddewan1235 <a.dewan.10@student.scu.edu.au>
2024-07-17 17:15:00 +1000

    Creating Objects

--- src/AssigmentOne.java
+++ src/AssigmentOne.java
@@ -1,6 +1,17 @@
 public class AssigmentOne {
     public static void main (String[] args)
     {
+// Part 3: creating objects for both the classes we made.
+        // 3 objects for GP
+        GeneralPractitioner gp1 = new GeneralPractitioner (1, "Awais Malangi", "General Practitioner", "Airport West");
+        GeneralPractitinore gp2 = new GeneralPractitioner (2, "Abdullah Ampol", "General Practitioner", "Kew Hospital");
+        GeneralPractitinore gp3 = new GeneralPractitinore (3, "Jalal Raami", "General Practitinore", "Diamond Creek");
+
+        // 2 objects for Specialists
+        Specialist specialist1 = new Specialist (1, "Amirah David", "Heart Specialist", "Cardiologist");
+        Specialist specialist2 = new Specialist (2, "Noori Iqbal", "Skin Specialist", "Dermatologist");
+
+        
 
     }
 }
