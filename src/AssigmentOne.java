10550a6368496ba5e5c3f9aa3cdb720f9f46a30b
Ahmeddewan1235 <a.dewan.10@student.scu.edu.au>
2024-07-17 17:33:26 +1000

    print code in main method

--- src/AssigmentOne.java
+++ src/AssigmentOne.java
@@ -11,7 +11,20 @@ public class AssigmentOne {
         Specialist specialist1 = new Specialist (1, "Amirah David", "Heart Specialist", "Cardiologist");
         Specialist specialist2 = new Specialist (2, "Noori Iqbal", "Skin Specialist", "Dermatologist");
 
-        
+        //Print for GP in Main Class
+        System.out.println ("Genral Practitioners are: ");
+        gp1.printDetails();
+        Sytem.out.println("__________");
+        gp2.printDetails();
+        Sytem.out.println("__________");
+        gp3.printDetails();
+        Sytem.out.println("__________");
 
+        //Print for Specialists in Mian Class
+        System.out.println ("Specialists are: ");
+        specialist1.printDetails();
+        Sytem.out.println("__________");
+        specialist2.printDetails();
+        Sytem.out.println("__________");
     }
 }
