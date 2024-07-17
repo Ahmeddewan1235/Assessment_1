27fafbd83763ebe69b558483d32acfceb460f705
Ahmeddewan1235 <a.dewan.10@student.scu.edu.au>
2024-07-17 16:30:36 +1000

    Specialists

--- src/Specialists.java
+++ src/Specialists.java
@@ -1,3 +1,22 @@
+
+
 public class Specialists extends HealthProfessional {
+    private String Field;
+
+    public Specialists(){
+        super();
+        this.Field = "Liver Specialists";
+    }
+
+    public Specialists(int ID, String Specialization, String Field){
+        super(ID, name, Specialization);
+        this.field = field;
+    }
+
+    public void printDetails(){
+        System.out.println("Specialist Doctor details: ");
+        super.printDetails();
+        System.out.println("Feild of :" + Field);
+    }
 }
 
